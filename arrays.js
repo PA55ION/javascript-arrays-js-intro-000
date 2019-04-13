c97d9f1458e464caa5fe700e73c4084cd24d3c05
Mai Campbell <jirallc@gmail.com>
2019-04-13 01:51:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,12 @@ function removeElementFromBeginningOfArray(array) {
     array = array.slice(1);
     return array;
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop();
+    return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  
+}
